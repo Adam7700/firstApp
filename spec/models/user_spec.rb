f9044d22207a4ef15b2f6d8fff748b0ad66196bf
Adam <adampog77@gmail.com>
2014-09-13 02:23:22 +0000

    added to user_spec.rb

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+
+	describe User do
+		let(:user) { User.new }
+		subject {user}
+		it { should respond_to(:name) }
+		it { should respond_to(:email) }
+		it { should respond_to(:password) }
+	end
+
+end
+
