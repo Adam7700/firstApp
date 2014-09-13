cc1b2f106040ec50cfbef4813bfbecbf06ea6a57
adam <adampog77@gmail.com>
2014-09-13 04:04:29 +0000

    updated

--- config/initializers/session_store.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-<<<<<<< HEAD
-Rails.application.config.session_store :cookie_store, key: '_myApp_session'
-=======
-Rails.application.config.session_store :cookie_store, key: '_firstApp_session'
->>>>>>> 9709c858c713aef57d706ced5d58c1cf52aa467b
