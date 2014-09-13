1380e8ccfcfa8057649a339c3b17308a50d05a94
adam <adampog77@gmail.com>
2014-09-13 19:52:26 +0000

    added tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+
+describe User do
+	let(:user) { User.new }
+	subject { user }
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+	it { should respond_to(:password) }
+	
+#	it { should be_valid }
+	
+	describe "empty name" do
+		before {user.name ='' }
+		it{should_not be_valid }
+	end
+	
+	describe "blank name" do
+		before {user.name =' ' }
+		it{should_not be_valid }
+	end
+
+	describe "empty email" do
+		before {user.email ='' }
+		it{should_not be_valid }
+	end
+	
+	describe "blank email" do
+		before {user.email =' ' }
+		it{should_not be_valid }
+	end
+
+	describe "empty password" do
+		before {user.password = '' }
+		it{should_not be_valid }
+	end
+	
+	describe "blank password" do
+		before {user.password = ' ' }
+		it{should_not be_valid }
+	end
+end
+
