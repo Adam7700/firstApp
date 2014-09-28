25f28088a7d57a2788c21557bd09a2f56a33772b
adam <adampog77@gmail.com>
2014-09-28 02:02:36 +0000

    completed log in/ log out

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -7,11 +7,18 @@ class LoginsController < ApplicationController
 		
 		if user && user.password == params[:password]
 			flash[:success] = "Logged in"
+			session[:user_id] = user.id
 			redirect_to users_path
 		else
 			flash.now[:danger] = "Invalid username or password"
 			render "new"
 		end
 		
+	end
+	
+	def destroy
+		flash[:info] = "Logged out"
+		session[:user_id] = nil
+		redirect_to users_path
 	end
 end
